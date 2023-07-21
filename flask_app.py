4741213c05e6424eb6a89209c21e24d53f5ba43a
Daniel Yip <daniel.yip.so@gmail.com>
2023-07-21 17:58:56 +0000

    connected database with created table

--- flask_app.py
+++ flask_app.py
@@ -9,15 +9,21 @@ app.config["DEBUG"] = True
 
 SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
     username="danielyip",
-    password="ab9g@A32",
+    password="Newpassword1",
     hostname="danielyip.mysql.pythonanywhere-services.com",
     databasename="danielyip$comments",
-    )
+)
 app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+
+class Comment(db.Model):
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(4096))
 
 comments = []
 
