835db9a0f4a717ba65645e80e7a1e55cc7999bdd
Daniel Yip <daniel.yip.so@gmail.com>
2023-07-21 13:29:06 +0000

    Connected MySQL database

--- flask_app.py
+++ flask_app.py
@@ -2,9 +2,22 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="danielyip",
+    password="ab9g@A32",
+    hostname="danielyip.mysql.pythonanywhere-services.com",
+    databasename="danielyip$comments",
+    )
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
 
 comments = []
 
