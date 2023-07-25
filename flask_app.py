1f0a0ebc1e7e4e1da33b3eaa8971d5e6d476ecd3
Daniel Yip <daniel.yip.so@gmail.com>
2023-07-25 05:36:06 +0000

    working log in page that checks credentials

--- flask_app.py
+++ flask_app.py
@@ -60,6 +60,12 @@ def calculator():
         return render_template("calculator.html")
 
 
-@app.route('/login/')
+@app.route('/login/', methods=["GET", "POST"])
 def login():
-        return render_template('login_page.html')
+    if request.method == "GET":
+        return render_template('login_page.html', error=False)
+
+    if request.form["username"] != "admin" or request.form["password"] != "secret":
+        return render_template("login_page.html", error=True)
+
+    return redirect(url_for('index'))
