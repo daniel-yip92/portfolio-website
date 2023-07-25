09b067e651cd95c62762f5523cc3c84fe084e47e
Daniel Yip <daniel.yip.so@gmail.com>
2023-07-25 07:04:27 +0000

    refined login page to accept list of users

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,11 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, redirect, render_template, request, url_for, json
+from flask import Flask, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_calculator import calculate
+from flask_login import login_user, LoginManager, UserMixin
+from werkzeug.security import check_password_hash, generate_password_hash
 import requests
 
 app = Flask(__name__)
@@ -20,6 +22,31 @@ app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+app.secret_key = "hammerrandomlyonsomekeys5"
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+class User(UserMixin):
+
+    def __init__(self, username, password_hash):
+        self.username = username
+        self.password_hash = password_hash
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def get_id(self):
+        return self.username
+
+all_users = {
+    "admin": User("admin", generate_password_hash("secret")),
+    "bob": User("bob", generate_password_hash("less-secret")),
+    "caroline": User("caroline", generate_password_hash("completely-secret")),
+    }
+
+@login_manager.user_loader
+def load_user(user_id):
+    return all_users.get(user_id)
 
 class Comment(db.Model):
     __tablename__ = "comments"
@@ -65,7 +92,13 @@ def login():
     if request.method == "GET":
         return render_template('login_page.html', error=False)
 
-    if request.form["username"] != "admin" or request.form["password"] != "secret":
+    username = request.form["username"]
+    if username not in all_users:
+        return render_template("login_page.html", error=True)
+    user = all_users[username]
+
+    if not user.check_password(request.form["password"]):
         return render_template("login_page.html", error=True)
 
+    login_user(user)
     return redirect(url_for('index'))
